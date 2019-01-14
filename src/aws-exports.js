ad17138418a60f1730e3c60fe2223d33c7f76186
Andreas Braumann <brew@brew.at>
2019-01-14 12:30:09 +0100

    - amplify hosting

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,10 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "eu-central-1:2b4cce46-bd81-4dcf-9f64-3aed81fe3214",
     "aws_cognito_region": "eu-central-1",
     "aws_user_pools_id": "eu-central-1_Gx8NKn0TL",
-    "aws_user_pools_web_client_id": "220ahrk7dinh374h8v3nnfqjv"
+    "aws_user_pools_web_client_id": "220ahrk7dinh374h8v3nnfqjv",
+    "aws_content_delivery_bucket": "xrsumerian-20190114122151-hostingbucket",
+    "aws_content_delivery_bucket_region": "eu-central-1",
+    "aws_content_delivery_url": "http://xrsumerian-20190114122151-hostingbucket.s3-website.eu-central-1.amazonaws.com"
 };
 
 
